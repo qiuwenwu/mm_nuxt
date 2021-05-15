8c93acc21c5c4ab8aa0aff99d6fd831a07e63807
邱文武 <573242395@qq.com>
2021-05-15 17:20:01 +0800

    更新

--- plugins/mm_component.js
+++ plugins/mm_component.js
@@ -1,4 +1,3 @@
-
 // 栏目
 import bar_action from '@/components/mm/bar/bar_action.vue';
 import bar_ad from '@/components/mm/bar/bar_ad.vue';
@@ -99,132 +98,202 @@ import swiper_text from '@/components/mm/swiper/swiper_text.vue';
 import table_coin from '@/components/mm/table/table_coin.vue';
 import table_stock from '@/components/mm/table/table_stock.vue';
 
+// 表格
+import expand_drag from '@/components/mm/expand/expand_drag.vue';
+import expand_sptite from '@/components/mm/expand/expand_sptite.vue';
+
 /* === 创建全局组件 === */
 export default {
-	install(Vue, options) {
+  install(Vue, options) {
 
-		// 栏目组件
-		Vue.component('bar_action', bar_action);
-		Vue.component('bar_ad', bar_ad);
-		Vue.component('bar_buy', bar_buy);
-		Vue.component('bar_chat', bar_chat);
-		Vue.component('bar_count', bar_count);
-		Vue.component('bar_filter', bar_filter);
-		Vue.component('bar_menu', bar_menu);
-		Vue.component('bar_search', bar_search);
-		Vue.component('bar_sort', bar_sort);
-		Vue.component('bar_tab', bar_tab);
-		Vue.component('bar_title', bar_title);
-		Vue.component('bar_tool', bar_tool);
-		Vue.component('bar_user', bar_user);
+    /**
+     * 转换时间
+     * @param {String} timeStr 时间字符串
+     * @param {String} format 转换格式
+     * @return {String} 返回转换后的结果
+     */
+    Vue.prototype.$to_time = function(timeStr, format) {
+      var time = timeStr.toTime();
+      if (format) {
+        return time.toStr(format);
+      } else {
+        var date = time.toStr("yyyy-MM-dd");
+        var now = new Date();
+        if (date == now.toStr("yyyy-MM-dd")) {
+          return time.toStr("hh:mm")
+        } else if (date == now.addDays(-1).toStr("yyyy-MM-dd")) {
+          return "昨天" + time.toStr("hh:mm")
+        } else if (time.toStr("yyyy") == now.toStr("yyyy")) {
+          return time.toStr("MM-dd");
+        } else {
+          return date;
+        }
+      }
+    };
 
-		// 列表项
-		Vue.component('item_address', item_address);
-		Vue.component('item_analyse', item_analyse);
-		Vue.component('item_article', item_article);
-		Vue.component('item_audio', item_audio);
-		Vue.component('item_base', item_base);
-		Vue.component('item_chat', item_chat);
-		Vue.component('item_comment', item_comment);
-		Vue.component('item_contact', item_contact);
-		Vue.component('item_count', item_count);
-		Vue.component('item_date', item_date);
-		Vue.component('item_forum', item_forum);
-		Vue.component('item_goods', item_goods);
-		Vue.component('item_image', item_image);
-		Vue.component('item_media', item_media);
-		Vue.component('item_message', item_message);
-		Vue.component('item_music', item_music);
-		Vue.component('item_news', item_news);
-		Vue.component('item_number', item_number);
-		Vue.component('item_order', item_order);
-		Vue.component('item_question', item_question);
-		Vue.component('item_recommend', item_recommend);
-		Vue.component('item_reply', item_reply);
-		Vue.component('item_tag', item_tag);
-		Vue.component('item_text', item_text);
-		Vue.component('item_thread', item_thread);
-		Vue.component('item_trend', item_trend);
-		Vue.component('item_type', item_type);
-		Vue.component('item_user', item_user);
-		Vue.component('item_video', item_video);
+    // 自定义拖动
+    Vue.directive('drag',
+      function(el, binding) {
+        el.onmousedown = function(e) {
+          let odiv = el; //获取当前元素
+          let firstTime = '',
+            lastTime = '';
+          // 给当前元素添加属性，用于元素状态的判断
+          odiv.setAttribute('ele-flag', false)
+          odiv.setAttribute('draging-flag', true)
 
-		// 列表
-		Vue.component('list_address', list_address);
-		Vue.component('list_analyse', list_analyse);
-		Vue.component('list_article', list_article);
-		Vue.component('list_audio', list_audio);
-		Vue.component('list_base', list_base);
-		Vue.component('list_chat', list_chat);
-		Vue.component('list_comment', list_comment);
-		Vue.component('list_contact', list_contact);
-		Vue.component('list_count', list_count);
-		Vue.component('list_date', list_date);
-		Vue.component('list_forum', list_forum);
-		Vue.component('list_goods', list_goods);
-		Vue.component('list_image', list_image);
-		Vue.component('list_media', list_media);
-		Vue.component('list_message', list_message);
-		Vue.component('list_music', list_music);
-		Vue.component('list_news', list_news);
-		Vue.component('list_number', list_number);
-		Vue.component('list_order', list_order);
-		Vue.component('list_question', list_question);
-		Vue.component('list_recommend', list_recommend);
-		Vue.component('list_reply', list_reply);
-		Vue.component('list_tag', list_tag);
-		Vue.component('list_text', list_text);
-		Vue.component('list_thread', list_thread);
-		Vue.component('list_trend', list_trend);
-		Vue.component('list_type', list_type);
-		Vue.component('list_user', list_user);
-		Vue.component('list_video', list_video);
+          e.preventDefault();
+          let bw = document.body.clientWidth;
+          let bh = document.body.clientHeight;
+          //鼠标按下，计算当前元素距离可视区的距离
+          let disX = e.clientX - el.offsetLeft;
+          let disY = e.clientY - el.offsetTop;
 
-		// 导航
-		Vue.component('nav_bottom', nav_bottom);
-		Vue.component('nav_main', nav_main);
-		Vue.component('nav_path', nav_path);
-		Vue.component('nav_quick', nav_quick);
-		Vue.component('nav_side', nav_side);
-		Vue.component('nav_top', nav_top);
-		Vue.component('nav_user', nav_user);
+          // 计算两边坐标
+          document.onmousemove = function(e) {
+            let l = 0,
+              t = 0;
+            // 拖动边界
+            if (e.clientX >= bw) {
+              l = bw - disX;
+            } else if (e.clientX <= 0) {
+              {
+                l = 0 - disX;
+              }
+            } else {
+              l = e.clientX - disX;
+            }
+            if (e.clientY >= bh) {
+              t = bh - disY;
+            } else if (e.clientY <= 0) {
+              t = 0 - disY;
+            } else {
+              t = e.clientY - disY;
+            }
+            //移动当前元素
+            el.style.left = l + 'px';
+            el.style.top = t + 'px';
+          };
+          // 鼠标停止移动时，事件移除
+          document.onmouseup = function(e) {
+            document.onmousemove = null;
+            document.onmouseup = null;
+            console.log({
+              x: e.clientX,
+              y: e.clientY
+            });
+            lastTime = new Date().getTime();
+            if ((lastTime - firstTime) > 200) {
+              odiv.setAttribute('ele-flag', true)
+              event.stopPropagation()
+            }
+            setTimeout(function() {
+              odiv.setAttribute('draging-flag', false)
+            }, 100)
+          };
+        };
+      }
+    );
+	
+    // 栏目组件
+    Vue.component('bar_action', bar_action);
+    Vue.component('bar_ad', bar_ad);
+    Vue.component('bar_buy', bar_buy);
+    Vue.component('bar_chat', bar_chat);
+    Vue.component('bar_count', bar_count);
+    Vue.component('bar_filter', bar_filter);
+    Vue.component('bar_menu', bar_menu);
+    Vue.component('bar_search', bar_search);
+    Vue.component('bar_sort', bar_sort);
+    Vue.component('bar_tab', bar_tab);
+    Vue.component('bar_title', bar_title);
+    Vue.component('bar_tool', bar_tool);
+    Vue.component('bar_user', bar_user);
 
-		// 轮播
-		Vue.component('swiper_card', swiper_card);
-		Vue.component('swiper_rotate_card', swiper_rotate_card);
-		Vue.component('swiper_image', swiper_image);
-		Vue.component('swiper_menu', swiper_menu);
-		Vue.component('swiper_page', swiper_page);
-		Vue.component('swiper_vertical_page', swiper_vertical_page);
-		Vue.component('swiper_text', swiper_text);
+    // 列表项
+    Vue.component('item_address', item_address);
+    Vue.component('item_analyse', item_analyse);
+    Vue.component('item_article', item_article);
+    Vue.component('item_audio', item_audio);
+    Vue.component('item_base', item_base);
+    Vue.component('item_chat', item_chat);
+    Vue.component('item_comment', item_comment);
+    Vue.component('item_contact', item_contact);
+    Vue.component('item_count', item_count);
+    Vue.component('item_date', item_date);
+    Vue.component('item_forum', item_forum);
+    Vue.component('item_goods', item_goods);
+    Vue.component('item_image', item_image);
+    Vue.component('item_media', item_media);
+    Vue.component('item_message', item_message);
+    Vue.component('item_music', item_music);
+    Vue.component('item_news', item_news);
+    Vue.component('item_number', item_number);
+    Vue.component('item_order', item_order);
+    Vue.component('item_question', item_question);
+    Vue.component('item_recommend', item_recommend);
+    Vue.component('item_reply', item_reply);
+    Vue.component('item_tag', item_tag);
+    Vue.component('item_text', item_text);
+    Vue.component('item_thread', item_thread);
+    Vue.component('item_trend', item_trend);
+    Vue.component('item_type', item_type);
+    Vue.component('item_user', item_user);
+    Vue.component('item_video', item_video);
 
-		/**
-		 * 转换时间
-		 * @param {String} timeStr 时间字符串
-		 * @param {String} format 转换格式
-		 * @return {String} 返回转换后的结果
-		 */
-		Vue.prototype.$to_time = function(timeStr, format){
-			var time = timeStr.toTime();
-			if(format){
-				return time.toStr(format);
-			}
-			else {
-				var date = time.toStr("yyyy-MM-dd");
-				var now = new Date();
-				if(date == now.toStr("yyyy-MM-dd")){
-					return time.toStr("hh:mm")
-				}
-				else if(date == now.addDays(-1).toStr("yyyy-MM-dd")){
-					return "昨天" + time.toStr("hh:mm")
-				}
-				else if(time.toStr("yyyy") == now.toStr("yyyy")){
-					return time.toStr("MM-dd");
-				}
-				else {
-					return date;
-				}
-			}
-		}
-	}
+    // 列表
+    Vue.component('list_address', list_address);
+    Vue.component('list_analyse', list_analyse);
+    Vue.component('list_article', list_article);
+    Vue.component('list_audio', list_audio);
+    Vue.component('list_base', list_base);
+    Vue.component('list_chat', list_chat);
+    Vue.component('list_comment', list_comment);
+    Vue.component('list_contact', list_contact);
+    Vue.component('list_count', list_count);
+    Vue.component('list_date', list_date);
+    Vue.component('list_forum', list_forum);
+    Vue.component('list_goods', list_goods);
+    Vue.component('list_image', list_image);
+    Vue.component('list_media', list_media);
+    Vue.component('list_message', list_message);
+    Vue.component('list_music', list_music);
+    Vue.component('list_news', list_news);
+    Vue.component('list_number', list_number);
+    Vue.component('list_order', list_order);
+    Vue.component('list_question', list_question);
+    Vue.component('list_recommend', list_recommend);
+    Vue.component('list_reply', list_reply);
+    Vue.component('list_tag', list_tag);
+    Vue.component('list_text', list_text);
+    Vue.component('list_thread', list_thread);
+    Vue.component('list_trend', list_trend);
+    Vue.component('list_type', list_type);
+    Vue.component('list_user', list_user);
+    Vue.component('list_video', list_video);
+
+    // 导航
+    Vue.component('nav_bottom', nav_bottom);
+    Vue.component('nav_main', nav_main);
+    Vue.component('nav_path', nav_path);
+    Vue.component('nav_quick', nav_quick);
+    Vue.component('nav_side', nav_side);
+    Vue.component('nav_top', nav_top);
+    Vue.component('nav_user', nav_user);
+
+    // 轮播
+    Vue.component('swiper_card', swiper_card);
+    Vue.component('swiper_rotate_card', swiper_rotate_card);
+    Vue.component('swiper_image', swiper_image);
+    Vue.component('swiper_menu', swiper_menu);
+    Vue.component('swiper_page', swiper_page);
+    Vue.component('swiper_vertical_page', swiper_vertical_page);
+    Vue.component('swiper_text', swiper_text);
+
+    //  拓展
+
+    Vue.component('expand_drag', expand_drag);
+    Vue.component('expand_sptite', expand_sptite);
+
+  }
 }
